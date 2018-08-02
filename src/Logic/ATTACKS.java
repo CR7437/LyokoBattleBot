0489cdf66879bbb82bea7f933c5e3d88ad6fcc0a
GoodOldJack12 <deloght.jonathan@gmail.com>
2018-08-03 01:27:39 +0200

    added classes and attacks

---
+++ src/Logic/ATTACKS.java
@@ -0,0 +1,28 @@
+package Logic;
+
+/**
+ * Created by jack on 8/3/18.
+ */
+public enum ATTACKS {
+    KATANASTRIKE(3,10),
+    IMPACT(1,5),
+    ENERGYSHIELD(1,5),
+    FANSTRIKE(1,5),
+    LASERARROW(1,5);
+
+
+    private final int damage;
+    private final int level;
+    ATTACKS(int level, int damage){
+        this.level = level;
+        this.damage = damage;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+}
---
+++ src/Logic/LYOKOCLASS.java
@@ -0,0 +1,41 @@
+package Logic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by jack on 8/3/18.
+ */
+public enum  LYOKOCLASS {
+    SAMURAI(Arrays.asList(ATTACKS.IMPACT,ATTACKS.KATANASTRIKE)),
+    FELINE(Arrays.asList(ATTACKS.LASERARROW)),
+    GEISHA(Arrays.asList(ATTACKS.FANSTRIKE)),
+    ANGEL(Arrays.asList(ATTACKS.ENERGYSHIELD));
+
+
+
+    private final List<ATTACKS> attacks;
+    LYOKOCLASS(List<ATTACKS> attacks){
+        this.attacks = attacks;
+    }
+    public List<ATTACKS> getAttacks(){
+        return attacks;
+    }
+
+    public List<ATTACKS> getLevelAttacks(int level){
+        List<ATTACKS> attacks = new ArrayList<>();
+        for (ATTACKS attack:getAttacks()) {
+            if (attack.getLevel() == level){
+                attacks.add(attack);
+            }
+        }
+        return attacks;
+    }
+
+
+
+
+
+}

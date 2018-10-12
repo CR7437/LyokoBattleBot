6d8fd6a42ec40d88cfdcfd69ec1a4665bd73fab3
TenDRILLL <32621403+TenDRILLL@users.noreply.github.com>
2018-10-12 23:40:18 +0300

    Divided command into commandName and args.

--- src/Discord/OnCommand.java
+++ src/Discord/OnCommand.java
@@ -7,26 +7,35 @@ import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.handle.obj.IUser;
 
+import java.util.Arrays;
+
 public class OnCommand {
+
+    public OnCommand(){
+        
+    }
+
     public void processCommand(IMessage message, String prefix){
         IUser sender = message.getAuthor();
         IChannel channel = message.getChannel();
         IGuild guild = message.getGuild();
 
         String[] command = message.getContent().replaceFirst(prefix, "").split(" ");
+        String commandName = command[0];
+        String[] args = Arrays.copyOfRange(command, 1, command.length);
 
-        if(command[0].equalsIgnoreCase("hello")) {
+        if(commandName.equalsIgnoreCase("hello")) {
             channel.sendMessage("Hai!");
-        } else if(command[0].equalsIgnoreCase("create")) {
+        } else if(commandName.equalsIgnoreCase("create")) {
             if(command.length < 2) {
                 channel.sendMessage("Invalid arguments. Usage: `" + prefix + "create name`");
             }
-            createCommand(channel,command);
-        }
+            createCommand(channel,args);
+        } else {channel.sendMessage("Invalid command.");}
     }
 
-    public void createCommand(IChannel channel, String[] command){
-        Lyokowarrior LW = new Lyokowarrior(command[1], LYOKOCLASS.SAMURAI);
+    public void createCommand(IChannel channel, String[] args){
+        Lyokowarrior LW = new Lyokowarrior(args[0], LYOKOCLASS.SAMURAI);
         channel.sendMessage(LW.getUsername() + " has been created.\nLevel: " + LW.getLevel() + " (" + LW.getXp() + " XP)");
         channel.sendMessage("Attacks: TO BE PRINT");
         LW.addXP(1500);

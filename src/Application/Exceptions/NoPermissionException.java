c381f66f84c57b86da22f61e3f205d7bec944220
GoodOldJack12 <deloght.jonathan@gmail.com>
2018-10-13 00:16:13 +0200

    added noPermissionException

---
+++ src/Application/Exceptions/NoPermissionException.java
@@ -0,0 +1,23 @@
+package Application.Exceptions;
+
+import sx.blah.discord.handle.impl.obj.Role;
+import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IUser;
+
+/**
+ * Created by jack on 10/13/18.
+ */
+public class NoPermissionException extends CommandException {
+    private final String roleName;
+    public NoPermissionException(IChannel channel, IUser user, String rolename) {
+        super(channel, user, String.format("You need to have the %s role to perform this command",rolename));
+        this.roleName = rolename;
+    }
+    public NoPermissionException(IChannel channel, IUser user, Role requiredRole){
+        this(channel,user,requiredRole.getName());
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+}
